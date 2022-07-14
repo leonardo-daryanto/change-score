674458291c79dd7589a0f422b866866e74d17a78
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-07-14 09:53:30 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
+namespace score {
+    /**
+     *  making a score and change. E.g. 23
+     */
+    //% block
+    //% group="Score Change"
+    export function changeScore(scoreOpened: boolean, changeScore: number): void {
+        
+        if (scoreOpened) {
+            
+            info.changeScoreBy(changeScore)
+        } else {
+            
+            info.setScore(0)
+        }
+    }
+    /**
+     *  clear the score (turn to 0)
+     */
+    //% block
+    //% group="Reset Score"
+    export function resetScore(): void {
 
+        info.setScore(0)
+    }
+}
